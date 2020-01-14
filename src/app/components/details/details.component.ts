660a318d512b2cee374f8e440df1601c38ef9188
Fabrizio <gianfranco.aguirre.cruz@gmail.com>
2020-01-14 11:03:30 -0500

    Se agrego recomendaciones

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { stringify } from 'querystring';
 import { url } from 'inspector';
+import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-details',
@@ -11,8 +12,8 @@ import { url } from 'inspector';
 })
 export class DetailsComponent implements OnInit {
   movie
+  recommendations
   id:string
-  url: string = 'https://api.themoviedb.org/3/movie/'
 
   constructor(private _route: ActivatedRoute, private __http: HttpClient) {
     
@@ -20,18 +21,26 @@ export class DetailsComponent implements OnInit {
 
   ngOnInit() {
     this.id = this._route.snapshot.paramMap.get('id')
-    this.url = this.url.concat(this.id,'?api_key=28a0d9072466fb61d9c60ead94c48450&language=en-US')
-    console.log(this.url.concat(this.id,'?api_key=28a0d9072466fb61d9c60ead94c48450&language=en-US'))
     this.findMovie()
+    this.findRecommendations()
   }
 
   findMovie(){
-    console.log("Hola")
-    this.__http.get(this.url,
+    this.__http.get('https://api.themoviedb.org/3/movie/'+this.id+'?api_key=28a0d9072466fb61d9c60ead94c48450&language=en-US',
     ).subscribe(
       data => {
         console.log(data)
         this.movie = data
+      }
+    )
+  }
+
+  findRecommendations(){
+    this.__http.get('https://api.themoviedb.org/3/movie/'+this.id+'/recommendations?api_key=28a0d9072466fb61d9c60ead94c48450&language=en-US&page=1',
+    ).subscribe(
+      data => {
+        console.log("hila")
+        this.recommendations = data.results
       }
     )
   }
