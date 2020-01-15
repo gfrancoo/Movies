2f7a0dda5034ae02ee8ebd9a614e5dd79da3c623
Fabrizio <gianfranco.aguirre.cruz@gmail.com>
2020-01-15 14:52:47 -0500

    Se implemento metodo post para valorar las peliculas

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { stringify } from 'querystring';
-import { url } from 'inspector';
-import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-details',
@@ -14,6 +11,7 @@ export class DetailsComponent implements OnInit {
   movie
   recommendations
   id:string
+  ratingMovie:number
 
   constructor(private _route: ActivatedRoute, private __http: HttpClient) {
     
@@ -23,10 +21,11 @@ export class DetailsComponent implements OnInit {
     this.id = this._route.snapshot.paramMap.get('id')
     this.findMovie()
     this.findRecommendations()
+    //this.rateMovie()
   }
 
   findMovie(){
-    this.__http.get('https://api.themoviedb.org/3/movie/'+this.id+'?api_key=28a0d9072466fb61d9c60ead94c48450&language=en-US',
+    this.__http.get('https://api.themoviedb.org/3/movie/' + this.id + '?api_key=28a0d9072466fb61d9c60ead94c48450&language=en-US',
     ).subscribe(
       data => {
         console.log(data)
@@ -36,9 +35,9 @@ export class DetailsComponent implements OnInit {
   }
 
   findRecommendations(){
-    this.__http.get('https://api.themoviedb.org/3/movie/'+this.id+'/recommendations?api_key=28a0d9072466fb61d9c60ead94c48450&language=en-US&page=1',
+    this.__http.get('https://api.themoviedb.org/3/movie/' + this.id + '/recommendations?api_key=28a0d9072466fb61d9c60ead94c48450&language=en-US&page=1',
     ).subscribe(
-      data => {
+      (data:any) => {
         this.recommendations = data.results
       }
     )
@@ -46,5 +45,33 @@ export class DetailsComponent implements OnInit {
 
   reloadPage(){
     this.ngOnInit()
+  }
+
+  postRateMovie(){
+    let url: string = "https://api.themoviedb.org/3/movie/"+ this.id +"/rating?api_key=28a0d9072466fb61d9c60ead94c48450"
+    let json = {
+      "value": this.ratingMovie
+    }
+    
+    this.__http.post(url,json).toPromise().then(
+      data=>{
+        console.log(data)
+      },
+      error=>{
+        console.log(error.error)
+      }
+    )
+  }
+
+  rateMovie(){
+    const checkRadioStar = document.querySelector(
+      'input[name="star"]:checked'
+    )
+    if(checkRadioStar == null) return
+    
+    this.ratingMovie = checkRadioStar.value
+    console.log(this.ratingMovie)
+
+    this.postRateMovie()
   }
 }
